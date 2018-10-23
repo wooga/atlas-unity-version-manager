27b506ad903d4848de9e244a14b24c7ba85b3a7a
Manfred Endres <2523575+Larusso@users.noreply.github.com>
2018-10-23 21:05:13 +0200

    Add native method detectProjectVersion (#2)
    Description
    ===========
    
    This patch implements the uvm method `String detectProjectVersion(File path)`. It returns
    the editor version used for a the project given as `path`. The
    implemention will by default search recursive downwards until it finds a
    _ProjectSettings_ directory with containing _ProjectVersion.txt_ file.
    If there is no unity project to be found or the version value in
    _ProjectVersion.txt_ is invalid the method returns `null`.
    
    This patch also introduces the provider pattern and a custom plugin
    extension to access the `uvm` API in a lazy fashion.
    
    > NOTE: These implementations are not yet final and might change without
    notice
    
    Changes
    =======
    
    ![ADD] `detectProjectVersion` native UnityVersionManager API
    ![ADD] draft plugin extension to wrap `uvm` API
    ![IMPROVE] jni project. Generate native headers while compiling

--- jni/rust/src/lib.rs
+++ jni/rust/src/lib.rs
@@ -1,10 +1,47 @@
 extern crate jni;
 #[macro_use]
 extern crate uvm_core;
+#[macro_use]
+extern crate error_chain;
 
 use jni::JNIEnv;
-use jni::objects::{JClass, JObject, };
+use jni::objects::{JClass, JObject, JValue};
 use jni::sys::{jstring};
+use std::path::{Path,PathBuf};
+
+mod error {
+    use jni;
+    use uvm_core;
+    use std;
+
+    error_chain! {
+        types {
+            UvmJniError, UvmJniErrorKind, ResultExt, UvmJniResult;
+        }
+
+        links {
+            Another(jni::errors::Error, jni::errors::ErrorKind) #[cfg(unix)];
+        }
+
+        foreign_links {
+            Uvm(uvm_core::error::UvmError);
+            Io(std::io::Error) #[cfg(unix)];
+        }
+    }
+}
+
+mod jni_utils {
+    use super::*;
+
+    /// Converts a `java.io.File` `JObject` into a `PathBuf`
+    pub fn get_path(env: &JNIEnv, path: JObject) -> error::UvmJniResult<PathBuf> {
+        env.call_method(path, "getPath", "()Ljava/lang/String;", &[])
+            .and_then(JValue::l)
+            .and_then(|object| env.get_string(object.into()))
+            .map(|p| Path::new(&String::from(p)).to_path_buf())
+            .map_err(|e| e.into())
+    }
+}
 
 #[no_mangle]
 #[allow(non_snake_case)]
@@ -12,4 +49,20 @@ pub extern "system" fn Java_net_wooga_uvm_UnityVersionManager_uvmVersion(env: JN
     env.new_string(cargo_version!())
         .map(|s| s.into_inner() )
         .unwrap_or_else(|_| JObject::null().into_inner())
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn Java_net_wooga_uvm_UnityVersionManager_detectProjectVersion<'a>(env: JNIEnv, _class: JClass, path: JObject) -> jstring {
+    jni_utils::get_path(&env,path)
+        .and_then(|path| {
+            uvm_core::dectect_project_version(&path,Some(true)).map_err(|e| e.into())
+        })
+        .and_then(|version| {
+            env.new_string(version.to_string()).map_err(|e| e.into())
+        })
+        .map(|s| s.into_inner() )
+        .unwrap_or_else(|_| {
+            JObject::null().into_inner()
+        })
 }
