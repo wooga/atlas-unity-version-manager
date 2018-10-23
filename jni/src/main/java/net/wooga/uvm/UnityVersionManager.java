5e2b7f15c5caddb4478e70ea008fa91b188afe6d
Manfred Endres <manfred.endres@tslarusso.de>
2018-10-23 09:57:21 +0200

    Add java doc to native java interface

--- jni/src/main/java/net/wooga/uvm/UnityVersionManager.java
+++ jni/src/main/java/net/wooga/uvm/UnityVersionManager.java
@@ -19,16 +19,26 @@ package net.wooga.uvm;
 
 import cz.adamh.utils.NativeUtils;
 import java.io.IOException;
+import java.util.logging.Logger;
 
+/**
+ * This is a simple native interface for the {@code unity version manager} tool.
+ * It loads the needed dynamic library from the resource directory at startup time.
+ */
 public class UnityVersionManager {
+
+    private final static Logger logger = Logger.getLogger(UnityVersionManager.class.getName());
 
     static {
         try {
             NativeUtils.loadLibraryFromJar("/native/" + System.mapLibraryName("uvm_jni"));
         } catch (IOException e) {
-
+            logger.warning("unable to load native library: " + System.mapLibraryName("uvm_jni"));
         }
     }
 
+    /**
+     * Returns the uvm core library version.
+     */
     public static native String uvmVersion();
 }
