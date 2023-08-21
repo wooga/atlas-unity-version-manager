b2cd16f5b69c0e189185116b27cdcdcece1f93ac
Joaquim Alvino de Mesquita Neto <joaquim.neto@wooga.net>
2023-08-21 15:58:11 +0200

    fix annotations

--- src/main/groovy/wooga/gradle/unity/version/manager/tasks/UvmCheckInstallation.groovy
+++ src/main/groovy/wooga/gradle/unity/version/manager/tasks/UvmCheckInstallation.groovy
@@ -28,6 +28,7 @@ import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.SetProperty
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
@@ -125,7 +126,7 @@ class UvmCheckInstallation extends DefaultTask implements BaseSpec {
 
     private final DirectoryProperty unityInstallBaseDir = objects.directoryProperty()
 
-    @Input
+    @InputDirectory
     DirectoryProperty getUnityInstallBaseDir() {
         unityInstallBaseDir
     }
